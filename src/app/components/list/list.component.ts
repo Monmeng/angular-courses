d41b4246594d8cb6ae9ba0265e56d4bb8e689a84
meng <843354450@qq.com>
2018-03-21 11:42:24 +0800

    add

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -14,29 +14,20 @@ export class ListComponent implements OnInit {
 
   ngOnInit() {
   }
-  num1=0;
-  num2=0;
   comlist=[];
   change(i){
     this.comlist.push(this.arrD[i])
     this.arrD.splice(i,1);
-    this.num1=this.arrD.length;
-    this.num2=this.comlist.length;
-    
   }
   change2(i){ 
     this.arrD.push(this.comlist[i])
     this.comlist.splice(i,1);
-    this.num1=this.arrD.length;
-    this.num2=this.comlist.length;
   }
   delData(i){
     this.arrD.splice(i,1);
-    this.num1=this.arrD.length;
   }
   delData2(i){
     this.comlist.splice(i,1);
-    this.num2= this.comlist.length; 
   }
   fn(i){
     //将下标抛出去
